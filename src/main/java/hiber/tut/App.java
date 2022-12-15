cd1bcbaeb372d655fd8fab447f2bebb44161fadc
Sumit Singh Rajput <sumitsinghrajput786gwalior@gmail.com>
2022-12-15 13:09:58 +0530

    Saving image and other type of value with hibernate

--- src/main/java/hiber/tut/App.java
+++ src/main/java/hiber/tut/App.java
@@ -1,7 +1,12 @@
 package hiber.tut;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -10,7 +15,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
     	Configuration cfg = new Configuration();
     	cfg.configure("hibernate.cfg.xml");
@@ -18,18 +23,32 @@ public class App
     	System.out.println(factory);
     	System.out.println(factory.isClosed());
     	
-    	//creating student
+    	//creating object of Student class
     	Student st = new Student();
     	st.setId(101);
     	st.setName("Sumit");
     	st.setCity("Gwalior");
     	System.out.println(st.toString());
     	
+    	//creating object of Address class
+    	Address ad = new Address();
+    	ad.setStreet("street1");
+    	ad.setCity("Delhi");
+    	ad.setOpen(true);
+    	ad.setAddedDate(new Date());
+    	ad.setX(1234.123);
+    	//Reading image
+    	FileInputStream fs = new FileInputStream("src/main/java/image.jpg");
+    	byte[] data = new byte[fs.available()];
+    	fs.read(data);
+    	ad.setImage(data);
     	///
     	Session session = factory.openSession();
-    	session.beginTransaction();
+    	Transaction tx =  session.beginTransaction();
     	session.save(st);
-    	session.getTransaction().commit();
+    	session.save(ad);
+    	tx.commit();
     	session.close();
+    	System.out.println("Done....");
     }
 }
