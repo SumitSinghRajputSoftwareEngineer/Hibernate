4d920691362e801cc4fe288bcd5d01e0b1cee092
Sumit Singh Rajput <sumitsinghrajput786gwalior@gmail.com>
2022-12-15 15:22:12 +0530

    Fetching object using get() method and load() method

---
+++ src/main/java/hiber/tut/fetchDemo.java
@@ -0,0 +1,22 @@
+package hiber.tut;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class fetchDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		
+		Student s = (Student)(session.get(Student.class, 101));
+		System.out.println(s.toString());
+		session.close();
+		factory.close();
+	}
+
+}
