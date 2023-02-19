522c58a93d3c79f2e8c3c1249e7ae538cb394a7d
Sumit Singh Rajput <sumitsinghrajput786gwalior@gmail.com>
2023-02-20 00:07:39 +0530

    HQL Complex Queries | Delete | Update | Join

--- src/main/java/hiber/hql/HQL.java
+++ src/main/java/hiber/hql/HQL.java
@@ -1,10 +1,12 @@
 package hiber.hql;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import hiber.tut.Student;
 
@@ -29,6 +31,30 @@ public class HQL {
 			System.out.println(student.getName());
 		}
 		
+		System.out.println("__________________ Delete Queries______________________________________");
+//		Transaction tx = s.beginTransaction();
+//		Query q2 = s.createQuery("delete from Student as s where s.city= :c");
+//		q2.setParameter("c", "Gwalior");
+//		int r  = q2.executeUpdate();
+//		System.out.println("Delete: " + r);
+//		tx.commit();
+		
+		System.out.println("_______________________Update Queries_________________________________________________");
+//		Transaction tx = s.beginTransaction();
+//		Query  q3 = s.createQuery("update Student set city=:c where name=:n");
+//		q3.setParameter("c", "Mumbai");
+//		q3.setParameter("n", "Sumit");
+//		System.out.println(q3.executeUpdate());
+//		tx.commit();
+//		
+		System.out.println("____________________Join_____________________________________");
+		//how to use INNER JOIN
+		Query q3 = s.createQuery("select q.question, q.questionId, a.answer from Question as q INNER JOIN q.answer as a");
+		
+		List<Object[]> list2 = q3.getResultList();
+		for(Object[] arr : list2) {
+			System.out.println(Arrays.toString(arr));
+		}
 		s.close();
 		factory.close();
 	}
